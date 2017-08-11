c33412ff84b4ffbf5c0d30b719179b1dc5cd7f65
wangtuo <wangtuo@zjrealtech.com>
2017-08-11 10:10:51 +0800

    zuul-gateway

---
+++ zuul-gateway/src/main/java/learn/springcloud/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,15 @@
+package learn.springcloud.zuulgateway;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {		
+		new SpringApplicationBuilder(ZuulGatewayApplication.class).web(true).run(args);
+	}
+}
